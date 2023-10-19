fdb22dee9ca102bd59cc9f87c10fec6ba482c064
LMontes94 <monteslucas1994@gmail.com>
2023-10-18 22:51:24 -0300

    pokeapi probando

--- promise/js/promise.js
+++ promise/js/promise.js
@@ -1,13 +1,15 @@
-
+const URL = "https://pokeapi.co/api/v2/pokemon/"
 // fetch devuelve una promise, al pasar por parametro un API
-fetch("https://pokeapi.co/api/v2/pokemon/1")
-// al recibir la promise hay que pasarla de json a un objeto js
-.then(res => res.json())
-// y luego trabajo con el objeto para mostrarlo
-.then(data => {
-    console.log(data.name);
-});
-
+for (let index = 1; index < 152; index++) {
+    fetch(URL + index)
+        // al recibir la promise hay que pasarla de json a un objeto js
+        .then(res => res.json())
+        // y luego trabajo con el objeto para mostrarlo
+        .then(data => {
+            console.log(data.name);
+            console.log(data);
+        });
+}
 function monstrarNombre(data) {
     data
 }
