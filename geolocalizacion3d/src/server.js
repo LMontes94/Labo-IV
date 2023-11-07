ad493a5d7d44fe86f2b037a38f819c575b193a65
LMontes94 <monteslucas1994@gmail.com>
2023-11-07 12:41:26 -0300

    geolocalizacion

---
+++ geolocalizacion3d/src/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// Define la carpeta donde se encuentran los archivos estáticos (en este caso, nivel superior)
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Configura una ruta para la página principal
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Resto de tu configuración...
+
+// Inicia el servidor en un puerto específico (por ejemplo, el puerto 3000)
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
+
