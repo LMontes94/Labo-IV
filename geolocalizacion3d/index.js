681c0b7f395153e4d29e978be810b7854aa9749d
LMontes94 <monteslucas1994@gmail.com>
2023-11-09 10:33:50 -0300

    probando

--- geolocalizacion3d/index.js
+++ geolocalizacion3d/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const cors = require('cors');
 
 // Define la carpeta donde se encuentran los archivos estáticos (en este caso, nivel superior)
 app.use(express.static(path.join(__dirname, './src/public')));
-
+app.use(cors());
 // Configura una ruta para la página principal
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './src/public', 'index.html'));
